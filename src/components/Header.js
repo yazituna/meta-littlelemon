5ff469a1bf78ecf2a3150f8aef8713641436dbc0
yazituna <akbulutt@yandex.com>
2023-10-26 20:49:41 +0300

    minor improvements

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,8 @@ import logo from '../assets/Logo.svg';
 const Header = (props) => {
   return (
     <header>
-      <div className="column">
-        <img src={logo} alt="Little Lemon Logo" width="360" />
+      <div className="column25">
+        <img src={logo} alt="Little Lemon Logo" className="header-logo" width="240" />
       </div>
       <div className="column-right">
         <Nav navs={props.navs}/>
